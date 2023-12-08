4e8abf326ced21bf345bb49796901c3d852e045a
JuanM4T <52723130+JuanM4T@users.noreply.github.com>
2023-12-08 18:00:54 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,104 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const config = require('./config.json');
+const { writeFile } = require('fs');
+
+const question = (query) => {
+    return new Promise((resolve) => {
+        rl.question(query, (answer) => {
+            resolve(answer);
+        });
+    });
+};
+const askCountry = async () => {
+    let answer = await question('Do you want to input a country? (y/n): ');
+    if(answer != '-1') config.country = answer == 'y' ? 1 : 0;
+    if(config.country){
+        answer = await question('What country code? (US, ES, JP, etc.): ');
+        if(answer != '-1') config.country = answer;
+    }
+    await askGamemode();
+};
+
+const askGamemode = async () => {
+    let answer = await question('What gamemode? (0 = osu, 1 = mania, 2 = taiko, 3 = catch): ');
+    if(answer != '-1') while(answer < 0 || answer > 3) answer = await question('What gamemode? (0 = osu, 1 = mania, 2 = taiko, 3 = catch): ');
+    if(answer != '-1') switch(answer){
+        case '0':
+            config.gamemode = 'osu';
+            break;
+        case '1':
+            config.gamemode = 'mania';
+            break;
+        case '2':
+            config.gamemode = 'taiko';
+            break;
+        case '3':
+            config.gamemode = 'fruits';
+            break;
+    }
+    await askStartDate();
+};
+
+const askStartDate = async () => {
+    let answer = await question("Do you want to input a start date (if you don't, you'll be asked for the amount of time) (y/n): ");
+    if(answer != '-1') config.time.absolute_s = answer == 'y' ? 1 : 0;
+    if(config.time.absolute_s){
+        answer = await question('What is the time.start date? (YYYY-MM-DD): ');
+        if(answer != '-1') config.time.start = Date.parse(answer);
+    }
+    await askFinishDate();
+};
+
+const askFinishDate = async () => {
+    let answer = await question("Do you want to input a time.finish date (if you don't, the program will check using the time and date you ran it as the finishing time) (y/n): ");
+    if(answer !== '-1') config.time.absolute_f = answer == 'y' ? 1 : 0;
+    if(config.time.absolute_f){
+        answer = await question('What is the time.finish date? (YYYY-MM-DD): ');
+        if(answer != '-1') config.time.finish = Date.parse(answer);
+    } else {
+        answer = await question('What is the length of the timeframe? (input number and type of length (y, m, w, d or h), for example 20d): ');
+        if(answer != '-1') config.time.finish = Date.now() - parseTime(answer);
+    }
+    await askPages();
+};
+
+const askPages = async () => {
+    let answer = await question('How many pages of ranking do you want to look? (1 page = 50 players): ');
+    if(answer !== '-1') config.pages = answer;
+};
+
+const parseTime = (timeString) => {
+    const value = parseInt(timeString);
+    const unit = timeString[timeString.length - 1];
+    const multiplier = unit === 'y' ? 365 * 24 * 60 * 60 * 1000 :
+                       unit === 'm' ? 30 * 24 * 60 * 60 * 1000 :
+                       unit === 'w' ? 7 * 24 * 60 * 60 * 1000 :
+                       unit === 'd' ? 24 * 60 * 60 * 1000 :
+                       unit === 'h' ? 60 * 60 * 1000 : 0;
+    return value * multiplier;
+}
+
+const questionArc = async () => {
+    config.onboarded != true ? console.log('Welcome to the onboarding process!\n') : console.log('Welcome to the config editor! You can skip questions by typing -1\n');
+    let answer = await question('What is your client id? (you can generate one of those on https://osu.ppy.sh/home/account/edit#oauth): ');
+    if(answer != '-1') config.api_key.client = answer;
+    answer = await question('What is your client secret?: ');
+    if(answer != '-1') config.api_key.secret = answer;
+    await askCountry();
+    config.onboarded = true;
+    writeFile('./config.json', JSON.stringify(config, null, "\t"), (err) => {
+        if (err) throw err;
+        console.log('Config saved! Remember that you can bring up this menu again by typing \'node config.js\'!');
+        rl.close();
+    });
+}
+
+questionArc();
+
+
+
+
---
+++ index.js
@@ -0,0 +1,92 @@
+const {v2, auth} = require('osu-api-extended')
+const config = require('./config.json');
+const login = async () => {
+  await auth.login(config.api_key.client, config.api_key.secret, ['public']);
+}
+
+const GetRanking = async () => {
+  const userIds = []
+  for(let i = 1; i <= config.pages; i++){
+    const ranking = await (await v2.site.ranking.details(config.gamemode, 'performance', {country: config.country, "cursor[page]": i})).ranking
+    for(user in ranking) {
+      userIds.push(ranking[user].user.id) 
+    }}
+  return Promise.resolve(userIds)
+}
+
+const RecentTopPlays = async (userIds) => {
+  const RecentTopPlays = []
+  for(let i = 0; i < userIds.length; i++){
+    const recentPlays = await v2.scores.user.category(userIds[i], 'best',{mode: config.gamemode, limit: 100})
+    const UsersTopPlays = []
+    for(item in recentPlays){
+      if(Date.parse(recentPlays[item].created_at) > config.time.finish - config.time.start){
+        UsersTopPlays.push(recentPlays[item])
+      }
+    }
+    RecentTopPlays.push(UsersTopPlays)
+    console.log("User " + (i+1) + " out of " + userIds.length + " done")
+  }
+  return Promise.resolve(RecentTopPlays)
+}
+
+//read out loud recent top plays
+// const readOutLoud = async (RecentTopPlays) => {
+//   for(let i = 0; i < RecentTopPlays.length; i++){
+//     for(let j = 0; j < RecentTopPlays[i].length; j++){
+//       console.log(RecentTopPlays[i][j].beatmap)
+//     }
+//   }
+// }
+
+const exportcsv = async (RecentTopPlays) => {
+  const fs = require('fs')
+  const csvWriter = require('csv-writer').createObjectCsvWriter({
+    path: 'out.csv',
+    header: [
+      {id: 'beatmap', title: 'Beatmap'},
+      {id: 'pp', title: 'PP'},
+      {id: 'accuracy', title: 'Accuracy'},
+      {id: 'max_combo', title: 'Max Combo'},
+      {id: 'mods', title: 'Mods'},
+      {id: 'rank', title: 'Rank'},
+      {id: 'user', title: 'User'},
+      {id: 'date', title: 'Date'},
+      {id: 'replay', title: 'Replay'},
+      {id: 'score', title: 'Score Link'},
+    ]
+  })
+  const records = []
+  for(let i = 0; i < RecentTopPlays.length; i++){
+    for(let j = 0; j < RecentTopPlays[i].length; j++){
+      records.push({
+        beatmap: "https://osu.ppy.sh/b/" + RecentTopPlays[i][j].beatmap.id,
+        pp: RecentTopPlays[i][j].pp,
+        accuracy: RecentTopPlays[i][j].accuracy,
+        max_combo: RecentTopPlays[i][j].max_combo,
+        mods: RecentTopPlays[i][j].mods,
+        rank: RecentTopPlays[i][j].rank,
+        user: RecentTopPlays[i][j].user.username,
+        date: RecentTopPlays[i][j].created_at,
+        replay: RecentTopPlays[i][j].replay,
+        score: "https://osu.ppy.sh/scores/" + config.gamemode + "/" + RecentTopPlays[i][j].id,
+      })
+    }
+  }
+  csvWriter.writeRecords(records)
+    .then(() => {
+      console.log('...Done')
+    })
+}
+
+const main = async () => {
+  if(!config.onboarded){ //read config.json parameters from the user
+    require('./config.js')
+  } else {
+  await login()
+  const userIds = await GetRanking()
+  const topPlays = await RecentTopPlays(userIds)
+  exportcsv(topPlays)
+}}
+
+main()
