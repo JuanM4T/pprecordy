f6045319e21006a22778d50d1eef0de3479446d5
JuanM4T <plmauna@gmail.com>
2024-12-17 17:17:45 +0100

    actual fix + remove pointless logging

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {v2, auth} = require('osu-api-extended')
+const { v2, auth } = require('osu-api-extended')
 const config = require('./config.json');
 const login = async () => {
   await auth.login(config.api_key.client, config.api_key.secret, ['public']);
@@ -6,24 +6,30 @@ const login = async () => {
 
 const GetRanking = async () => {
   const userIds = []
-  for(let i = 1; i <= config.pages; i++){
-    const ranking = await (await v2.site.ranking.details(config.gamemode, 'performance', {country: config.country, "cursor[page]": i})).ranking
-    for(user in ranking) {
-      userIds.push(ranking[user].user.id) 
-    }}
+  for (let i = 1; i <= config.pages; i++) {
+    const ranking = await (await v2.site.ranking.details(config.gamemode, 'performance', { country: config.country, "cursor[page]": i })).ranking
+    for (user in ranking) {
+      userIds.push(ranking[user].user.id)
+    }
+  }
   return Promise.resolve(userIds)
 }
 
 const RecentTopPlays = async (userIds) => {
   const RecentTopPlays = []
-  for(let i = 0; i < userIds.length; i++){
-    const recentPlays = await v2.scores.user.category(userIds[i], 'best', {mode: config.gamemode, limit: 100}).filter(
-      (score) => score.created_at >= config.time.start && (!config.time.absolute_f || score.created_at <= config.time.finish)
+  for (let i = 0; i < userIds.length; i++) {
+    const recentPlays = await v2.scores.user.category(userIds[i], 'best', { mode: config.gamemode, limit: 100 });
+    RecentTopPlays.push(
+      recentPlays.filter(
+        (score) => {
+          const scoreCreationTimestamp = Date.parse(score.created_at);
+          return scoreCreationTimestamp >= config.time.start && (!config.time.absolute_f || scoreCreationTimestamp <= config.time.finish)
+        }
+      )
     )
-    RecentTopPlays.push(RecentPlays)
-    console.log("User " + (i+1) + " out of " + userIds.length + " done")
+    //console.log("User " + (i + 1) + " out of " + userIds.length + " done")
   }
-  return Promise.resolve(RecentTopPlays)
+  return Promise.resolve(RecentTopPlays);
 }
 
 //read out loud recent top plays
@@ -40,21 +46,21 @@ const exportcsv = async (RecentTopPlays) => {
   const csvWriter = require('csv-writer').createObjectCsvWriter({
     path: 'out.csv',
     header: [
-      {id: 'beatmap', title: 'Beatmap'},
-      {id: 'pp', title: 'PP'},
-      {id: 'accuracy', title: 'Accuracy'},
-      {id: 'max_combo', title: 'Max Combo'},
-      {id: 'mods', title: 'Mods'},
-      {id: 'rank', title: 'Rank'},
-      {id: 'user', title: 'User'},
-      {id: 'date', title: 'Date'},
-      {id: 'replay', title: 'Replay'},
-      {id: 'score', title: 'Score Link'},
+      { id: 'beatmap', title: 'Beatmap' },
+      { id: 'pp', title: 'PP' },
+      { id: 'accuracy', title: 'Accuracy' },
+      { id: 'max_combo', title: 'Max Combo' },
+      { id: 'mods', title: 'Mods' },
+      { id: 'rank', title: 'Rank' },
+      { id: 'user', title: 'User' },
+      { id: 'date', title: 'Date' },
+      { id: 'replay', title: 'Replay' },
+      { id: 'score', title: 'Score Link' },
     ]
   })
   const records = []
-  for(let i = 0; i < RecentTopPlays.length; i++){
-    for(let j = 0; j < RecentTopPlays[i].length; j++){
+  for (let i = 0; i < RecentTopPlays.length; i++) {
+    for (let j = 0; j < RecentTopPlays[i].length; j++) {
       records.push({
         beatmap: "https://osu.ppy.sh/b/" + RecentTopPlays[i][j].beatmap.id,
         pp: RecentTopPlays[i][j].pp,
@@ -76,7 +82,7 @@ const exportcsv = async (RecentTopPlays) => {
 }
 
 const main = async () => {
-  if(!config.onboarded){ //read config.json parameters from the user
+  if (!config.onboarded) { //read config.json parameters from the user
     require('./config.js')
   } else {
     (config.time.finish ? "" : config.time.finish = Date.now())
@@ -84,6 +90,7 @@ const main = async () => {
     const userIds = await GetRanking()
     const topPlays = await RecentTopPlays(userIds)
     exportcsv(topPlays)
-}}
+  }
+}
 
 main()
